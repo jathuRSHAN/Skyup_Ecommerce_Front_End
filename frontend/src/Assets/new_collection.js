b7e8554f130fee6a13c5c80651651c445a8ce2c2
Wasana Karunasena <141900162+WasanaKarunasena@users.noreply.github.com>
2025-04-12 11:46:55 +0530

    Add files via upload

---
+++ frontend/src/Assets/new_collection.js
@@ -0,0 +1,76 @@
+import p5_img from "./product_5.jpg";
+import p6_img from "./product_6.jpg";
+import p7_img from "./product_7.jpg";
+import p14_img from "./product_14.jpg";
+import p15_img from "./product_15.jpg";
+import p16_img from "./product_16.jpg";
+import p25_img from "./product_25.jpg";
+import p26_img from "./product_26.jpg";
+
+let new_collections = [
+  {
+    id: 5,
+    name: "Black Shark 5 Pro",
+    image: p5_img,
+    new_price: 849.0,
+    old_price: 1049.0,
+
+  },
+  {
+    id: 6,
+    name: "ASUS ROG Phone 6D",
+    image: p6_img,
+    new_price: 899.0,
+    old_price: 1099.0,
+
+  },
+  {
+    id: 7,
+    name: "Poco F4 GT",
+    image: p7_img,
+    new_price: 599.0,
+    old_price: 749.0,
+  },
+  {
+    id: 14,
+    name: "iPhone 15 Pro Max",
+    image: p14_img,
+    new_price: 1199.0,
+    old_price: 1399.0,
+
+  },
+  {
+    id: 15,
+    name: "Samsung Galaxy Note20 Ultra",
+    image: p15_img,
+    new_price: 1099.0,
+    old_price: 1299.0,
+
+  },
+  {
+    id: 16,
+    name: "Huawei Mate X3",
+    image: p16_img,
+    new_price: 1699.0,
+    old_price: 1899.0,
+
+  },
+  {
+    id: 25,
+    name: "Redmi Note 12",
+    image: p25_img,
+    new_price: 199.0,
+    old_price: 399.0,
+
+  },
+  {
+    id: 26,
+    name: "Realme Narzo 50",
+    image: p26_img,
+    new_price: 179.0,
+    old_price: 379.0,
+
+  },
+];
+
+export default new_collections;
