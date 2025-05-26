f089614a9891e99fdfd9b729e694f04d7fc312ff
Wasana Karunasena <141900162+WasanaKarunasena@users.noreply.github.com>
2025-05-26 20:08:13 +0530

    Update ProductDisplay.jsx

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './ProductDisplay.css';
 import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from '../Assets/star_dull_icon.png';
@@ -7,12 +7,25 @@ import { ShopContext } from '../../Context/ShopContext';
 const ProductDisplay = (props) => {
     const { product } = props;
     const { addToCart } = useContext(ShopContext);
+    const [showNotification, setShowNotification] = useState(false);
+
+    const handleAddToCart = () => {
+        addToCart(product._id);
+        setShowNotification(true);
+        setTimeout(() => {
+            setShowNotification(false);
+        }, 2000); 
+    };
 
     return (
         <div className='productdisplay'>
+            {showNotification && (
+                <div className="custom-notification">
+                    ✅ <strong>{product.name}</strong> added to cart!
+                </div>
+            )}
             <div className="productdisplay-left">
                 <div className="productdisplay-img-list">
-                    {/* Ensure different images for thumbnails if available */}
                     <img src={product.image} alt="" />
                     <img src={product.image} alt="" />
                     <img src={product.image} alt="" />
@@ -32,31 +45,30 @@ const ProductDisplay = (props) => {
                     <img src={star_dull_icon} alt="" />
                     <p>(122)</p>
                 </div>
-                <div classname="productdisplay-right-prices">
+                <div className="productdisplay-right-prices">
                     <div className="productdisplay-right-price-old">${product.old_price}</div>
                     <div className="productdisplay-right-price-new">${product.new_price}</div>
-                    <div className="productdisplay-right-description">
-                        A light weight 256GB LED Screen smartphone.
-                    </div>
-                    <div className="productdisplay-right-size">
-                        <h1>Select Size</h1>
-                        <div className="productdisplay-right-sizes">
-                            <div>32GB</div>
-                            <div>64GB</div>
-                            <div>128GB</div>
-                            <div>256GB</div>
-                            <div>1TB</div>
-                        </div>
-                    </div>
-                    {/* Update to use _id for cart functionality */}
-                    <button onClick={() => { addToCart(product._id) }}>ADD TO CART</button>
-                    <p className='productdisplay-right-category'>
-                        <span>Category :</span> Smartphones, Gaming Phones, Phablets, Budget Phones
-                    </p>
-                    <p className='productdisplay-right-category'>
-                        <span>Tags :</span> Modern, Latest, Limited Edition
-                    </p>
                 </div>
+                <div className="productdisplay-right-description">
+                    A lightweight 256GB LED Screen smartphone.
+                </div>
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-sizes">
+                        <div>32GB</div>
+                        <div>64GB</div>
+                        <div>128GB</div>
+                        <div>256GB</div>
+                        <div>1TB</div>
+                    </div>
+                </div>
+                <button onClick={handleAddToCart}>ADD TO CART</button>
+                <p className='productdisplay-right-category'>
+                    <span>Category :</span> Smartphones, Gaming Phones, Phablets, Budget Phones
+                </p>
+                <p className='productdisplay-right-category'>
+                    <span>Tags :</span> Modern, Latest, Limited Edition
+                </p>
             </div>
         </div>
     );
