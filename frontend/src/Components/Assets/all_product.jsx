3c6384b570235afeccbf0848c89e2816000cd2b7
Wasana Karunasena <141900162+WasanaKarunasena@users.noreply.github.com>
2025-06-11 20:25:34 +0530

    Add files via upload

---
+++ frontend/src/Components/Assets/all_product.jsx
@@ -0,0 +1,337 @@
+import p1_img from "./product_1.jpg";
+import p2_img from "./product_2.jpg";
+import p3_img from "./product_3.jpg";
+import p4_img from "./product_4.jpg";
+import p5_img from "./product_5.jpg";
+import p6_img from "./product_6.jpg";
+import p7_img from "./product_7.jpg";
+import p8_img from "./product_8.jpg";
+import p9_img from "./product_9.jpg";
+import p10_img from "./product_10.jpg";
+import p11_img from "./product_11.jpg";
+import p12_img from "./product_12.jpg";
+import p13_img from "./product_13.jpg";
+import p14_img from "./product_14.jpg";
+import p15_img from "./product_15.jpg";
+import p16_img from "./product_16.jpg";
+import p17_img from "./product_17.jpg";
+import p18_img from "./product_18.jpg";
+import p19_img from "./product_19.jpg";
+import p20_img from "./product_20.jpg";
+import p21_img from "./product_21.jpg";
+import p22_img from "./product_22.jpg";
+import p23_img from "./product_23.jpg";
+import p24_img from "./product_24.jpg";
+import p25_img from "./product_25.jpg";
+import p26_img from "./product_26.jpg";
+import p27_img from "./product_27.jpg";
+import p28_img from "./product_28.jpg";
+import p29_img from "./product_29.jpg";
+import p30_img from "./product_30.jpg";
+import p31_img from "./product_31.jpg";
+import p32_img from "./product_32.jpg";
+import p33_img from "./product_33.jpg";
+import p34_img from "./product_34.jpg";
+import p35_img from "./product_35.jpg";
+import p36_img from "./product_36.jpg";
+
+// --- Gaming Phones ---
+const all_product = [
+  {
+    id: 1,
+    name: "Red Magic 8 Pro",
+    image: p1_img,
+    new_price: 799.0,
+    old_price: 999.0,
+    category: "gaming",
+  },
+  {
+    id: 2,
+    name: "ASUS ROG Phone 7",
+    image: p2_img,
+    new_price: 999.0,
+    old_price: 1199.0,
+    category: "gaming",
+  },
+  {
+    id: 3,
+    name: "Lenovo Legion Duel 2",
+    image: p3_img,
+    new_price: 899.0,
+    old_price: 1099.0,
+    category: "gaming",
+  },
+  {
+    id: 4,
+    name: "Nubia Red Magic 6R",
+    image: p4_img,
+    new_price: 699.0,
+    old_price: 899.0,
+    category: "gaming",
+  },
+  {
+    id: 5,
+    name: "Black Shark 5 Pro",
+    image: p5_img,
+    new_price: 849.0,
+    old_price: 1049.0,
+    category: "gaming",
+  },
+  {
+    id: 6,
+    name: "ASUS ROG Phone 6D",
+    image: p6_img,
+    new_price: 899.0,
+    old_price: 1099.0,
+    category: "gaming",
+  },
+  {
+    id: 7,
+    name: "Poco F4 GT",
+    image: p7_img,
+    new_price: 599.0,
+    old_price: 749.0,
+    category: "gaming",
+  },
+  {
+    id: 8,
+    name: "ZTE Axon 40 Ultra",
+    image: p8_img,
+    new_price: 799.0,
+    old_price: 949.0,
+    category: "gaming",
+  },
+  {
+    id: 9,
+    name: "iQOO 9 Pro",
+    image: p9_img,
+    new_price: 749.0,
+    old_price: 949.0,
+    category: "gaming",
+  },
+  {
+    id: 10,
+    name: "Realme GT Neo 3",
+    image: p10_img,
+    new_price: 499.0,
+    old_price: 699.0,
+    category: "gaming",
+  },
+  {
+    id: 11,
+    name: "Red Magic 9 Pro",
+    image: p11_img,
+    new_price: 849.0,
+    old_price: 1049.0,
+    category: "gaming",
+  },
+  {
+    id: 12,
+    name: "ASUS ROG Phone 8",
+    image: p12_img,
+    new_price: 999.0,
+    old_price: 1199.0,
+    category: "gaming",
+  },
+
+  // --- Phablet Phones ---
+  {
+    id: 13,
+    name: "Samsung Galaxy Z Fold5",
+    image: p13_img,
+    new_price: 1799.0,
+    old_price: 1999.0,
+    category: "phablet",
+  },
+  {
+    id: 14,
+    name: "iPhone 15 Pro Max",
+    image: p14_img,
+    new_price: 1199.0,
+    old_price: 1399.0,
+    category: "phablet",
+  },
+  {
+    id: 15,
+    name: "Samsung Galaxy Note20 Ultra",
+    image: p15_img,
+    new_price: 1099.0,
+    old_price: 1299.0,
+    category: "phablet",
+  },
+  {
+    id: 16,
+    name: "Huawei Mate X3",
+    image: p16_img,
+    new_price: 1699.0,
+    old_price: 1899.0,
+    category: "phablet",
+  },
+  {
+    id: 17,
+    name: "Oppo Find N2",
+    image: p17_img,
+    new_price: 1599.0,
+    old_price: 1799.0,
+    category: "phablet",
+  },
+  {
+    id: 18,
+    name: "Xiaomi Mix Fold 2",
+    image: p18_img,
+    new_price: 1499.0,
+    old_price: 1699.0,
+    category: "phablet",
+  },
+  {
+    id: 19,
+    name: "Motorola Razr 40 Ultra",
+    image: p19_img,
+    new_price: 1399.0,
+    old_price: 1599.0,
+    category: "phablet",
+  },
+  {
+    id: 20,
+    name: "Sony Xperia 1 V",
+    image: p20_img,
+    new_price: 1299.0,
+    old_price: 1499.0,
+    category: "phablet",
+  },
+  {
+    id: 21,
+    name: "Honor Magic V2",
+    image: p21_img,
+    new_price: 1399.0,
+    old_price: 1599.0,
+    category: "phablet",
+  },
+  {
+    id: 22,
+    name: "Vivo X Fold",
+    image: p22_img,
+    new_price: 1349.0,
+    old_price: 1549.0,
+    category: "phablet",
+  },
+  {
+    id: 23,
+    name: "OnePlus Open",
+    image: p23_img,
+    new_price: 1499.0,
+    old_price: 1699.0,
+    category: "phablet",
+  },
+  {
+    id: 24,
+    name: "Google Pixel Fold",
+    image: p24_img,
+    new_price: 1599.0,
+    old_price: 1799.0,
+    category: "phablet",
+  },
+
+  // --- Budget Phones ---
+  {
+    id: 25,
+    name: "Redmi Note 12",
+    image: p25_img,
+    new_price: 199.0,
+    old_price: 399.0,
+    category: "budget",
+  },
+  {
+    id: 26,
+    name: "Realme Narzo 50",
+    image: p26_img,
+    new_price: 179.0,
+    old_price: 379.0,
+    category: "budget",
+  },
+  {
+    id: 27,
+    name: "Samsung Galaxy A14",
+    image: p27_img,
+    new_price: 149.0,
+    old_price: 349.0,
+    category: "budget",
+  },
+  {
+    id: 28,
+    name: "Infinix Zero 5G",
+    image: p28_img,
+    new_price: 189.0,
+    old_price: 299.0,
+    category: "budget",
+  },
+  {
+    id: 29,
+    name: "Tecno Pova 5",
+    image: p29_img,
+    new_price: 179.0,
+    old_price: 279.0,
+    category: "budget",
+  },
+  {
+    id: 30,
+    name: "Motorola G73",
+    image: p30_img,
+    new_price: 199.0,
+    old_price: 299.0,
+    category: "budget",
+  },
+  {
+    id: 31,
+    name: "Lava Blaze 5G",
+    image: p31_img,
+    new_price: 169.0,
+    old_price: 269.0,
+    category: "budget",
+  },
+  {
+    id: 32,
+    name: "iTel P40",
+    image: p32_img,
+    new_price: 129.0,
+    old_price: 199.0,
+    category: "budget",
+  },
+  {
+    id: 33,
+    name: "Nokia G21",
+    image: p33_img,
+    new_price: 149.0,
+    old_price: 229.0,
+    category: "budget",
+  },
+  {
+    id: 34,
+    name: "Micromax IN Note 2",
+    image: p34_img,
+    new_price: 159.0,
+    old_price: 239.0,
+    category: "budget",
+  },
+  {
+    id: 35,
+    name: "Infinix Hot 30",
+    image: p35_img,
+    new_price: 139.0,
+    old_price: 219.0,
+    category: "budget",
+  },
+  {
+    id: 36,
+    name: "Realme C55",
+    image: p36_img,
+    new_price: 149.0,
+    old_price: 229.0,
+    category: "budget",
+  },
+];
+
+export default all_product;
+
+
+
